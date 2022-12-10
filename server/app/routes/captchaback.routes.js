537e5f4c292c439aad45bf7e565f4bed744c6feb
Kirill Mozhaev <mozhaevka2@gmail.com>
2022-12-10 23:45:24 +0500

    убрать лишнее

--- server/app/routes/captchaback.routes.js
+++
@@ -1,9 +0,0 @@
-module.exports = app => {
-    const captchabacks = require("../controllers/captchaback.controller.js");
-  
-    var router = require("express").Router();
-
-    router.post("/", captchabacks.captchasend);
-  
-    app.use('/api/captchabacks', router);
-  };
